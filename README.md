fa5ab89017e2b1f3d92e4b4cdb3f509bd2e26634
joseneto <jose.lopes.santos.neto@gmail.com>
2023-11-02 10:06:52 -0300

    Updating README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rails Tickets
+A ROR simple and objective application to control tickets for a Project company.
+Inspired by the simplicity of https://sifterapp.com
+
+# Models
+
+![ERD](erd.png?raw=true "ERD")
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-    belongs_to :company
-    has_many :administered_accounts, :class_name => 'Account', :foreign_key => 'admin_user_id'
-    has_many :project_accesses
-    has_many :opened_issues, :class_name => 'Issue', :foreign_key => 'opener_user_id'
-    has_many :assigned_issues, :class_name => 'Issue', :foreign_key => 'assignee_user_id'
-    has_many :responses, :foreign_key => 'responder_user_id'
-    has_many :default_assignments, :class_name => 'Category', :foreign_key => 'default_assignee_id'
+  belongs_to :company
+  has_many :administered_accounts, :class_name => 'Account', :foreign_key => 'admin_user_id'
+  has_many :project_accesses
+  has_many :opened_issues, :class_name => 'Issue', :foreign_key => 'opener_user_id'
+  has_many :assigned_issues, :class_name => 'Issue', :foreign_key => 'assignee_user_id'
+  has_many :responses, :foreign_key => 'responder_user_id'
+  has_many :default_assignments, :class_name => 'Category', :foreign_key => 'default_assignee_id'
 
 end
